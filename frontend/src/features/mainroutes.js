a384206b6562af870553c2e9dcc1a5b76eed414f
Bennett <BaharaJr>
2019-05-19 19:43:36 +0300

    chore: Update the routes to add driver profile

--- frontend/src/features/mainroutes.js
+++ frontend/src/features/mainroutes.js
@@ -3,12 +3,16 @@ import HomePage from './home';
 import {Route, Switch} from 'react-router-dom';
 import Profile from './profile';
 import Request from './request';
+import SignupDriver from './signupdriver';
+import Driver from './driver';
 
 const MainRoutes = () => (
     <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/profile" component={Profile} />
         <Route path="/request" component={Request} />
+        <Route path="/signupdriver" component={SignupDriver} />
+        <Route path="/driver" component={Driver} />
     </Switch>
 )
 
