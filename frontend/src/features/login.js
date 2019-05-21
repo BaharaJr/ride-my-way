b774ac575af9058b12faaf844a9b35d1cb4d035a
Bennett <BaharaJr>
2019-05-21 10:53:07 +0300

    feat(login): Change login modal to login page I changed the login modal to be the login page See #14

---
+++ frontend/src/features/login.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react';
+import './landing.css';
+import { Link } from 'react-router-dom';
+// import Background from './media/ride';
+
+class Login extends Component {
+    constuctor() {
+        this.handlePageChange = this.handlePageChange.bind(this);
+        this.handleRouteChange = this.handleRouteChange.bind(this);
+    }
+    handlePageChange() {
+        window.location= "/profile";
+    }
+
+    render() {
+        return (
+            <div>
+                <div class="log">
+                    <Link to="/">
+                        <p> Ride My Way</p>
+                    </Link>
+                </div>
+                <div className="login-form-wrapper modal">
+                    </div>
+                    <div className="form-header">
+                        <h1>Welcome Back</h1>
+                        <h3>Happy to See you Again</h3>
+                    </div>
+                    <form className="form">
+                        <div className="input-group">
+                            <i className="fas fa-user-check"></i>
+                            <input type="text" placeholder="Username" />
+                        </div>
+                        <div className="input-group">
+                            <i className="fas fa-key"></i>
+                            <input type="password" placeholder="Password" />
+                        </div>
+                        <h1 onClick={this.handlePageChange}> <Link to="/profile.js" ></Link>LogIn</h1>
+
+                    </form>
+                </div>
+        )
+    }
+}
+
+export default Login;
