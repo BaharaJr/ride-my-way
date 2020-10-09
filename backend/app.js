67528c4dd8ce797b0792989dfdf9581bfc420d9a
Bennett <bennybenester@gmail.com>
2020-10-09 15:50:12 +0300

    do enough refactoring

--- backend/app.js
+++ backend/app.js
@@ -2,31 +2,31 @@
 import express from 'express';
 import logger from 'morgan';
 import cookieParser from 'cookie-parser';
-import { json, urlencoded } from 'body-parser';
 import cors from 'cors';
 import http from 'http';
-import router from './routes/index';
 import chalk from 'chalk';
-import "@babel/polyfill";
+import '@babel/polyfill';
 import swaggerUi from 'swagger-ui-express';
 import YAML from 'yamljs';
-const swaggerDocument = YAML.load('./api/swagger/swagger.yaml');
+import router from './src/routes/index';
+
+const swaggerDocument = YAML.load('./src/api/swagger/swagger.yaml');
 
 
 const app = express();
 
-const hostname = 'localhost';
-const port = process.env.PORT || 5000;
+const port = process.env.PORT;
 const server = http.createServer(app);
 
 const bodyParser = require('body-parser');
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
-  extended: true
+  extended: true,
 }));
 
-var options = {
-  customCss: '.swagger-ui .topbar { display: none }'
+const options = {
+  customCss: '.swagger-ui .topbar { display: none }',
 };
 
 app.use(express.json());
@@ -36,21 +36,6 @@ app.use(cors());
 app.use(router);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, options));
 
-// catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error('Something went wrong. Be assured we\'re working on it!');
-  err.status = 404;
-  next(err);
-});
-
-// error handler
-app.use((err, req, res, next) => {
-  res.status(err.status || 500);
-  res.json({
-    message: err.message,
-    error: req.app.get('env') === 'development' ? err : {},
-  });
-});
 server.listen(port, () => {
   console.log(chalk.yellow('App is Live'));
 });
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,9 +1,18 @@
-import express from 'express';
+import { Router } from 'express';
+import swaggerUi from 'swagger-ui-express';
+import Rides from '../controllers/rides';
+import User from '../auth/index';
 
-const router = express.Router();
+const router = Router();
 
-router.get('/', (req, res, next) => {
-  res.render('index', { title: 'Express' });
+router.post('/auth/register', User.register);
+router.post('/auth/login', User.login);
+router.get('/rides', Rides.getAll);
+router.get('/rides/:id', Rides.getOne);
+router.post('/users/rides', Rides.create);
+router.get('/', (req, res) => {
+  res.send('APIS');
 });
+router.use('/', swaggerUi.serve);
 
 export default router;
